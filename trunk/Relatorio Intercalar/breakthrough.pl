97642afc9c8feaecc81a5af0fd036e8406ad945b
joaoh88 <joaoh88@4a0a0104-13ad-29fb-b2ae-627e6a0b3170>
2011-10-02 16:37:58 +0000

    mais fixes na formatação do template, e descrição dos predicados no ponto 5.

--- trunk/Relatorio Intercalar/breakthrough.pl
+++ trunk/Relatorio Intercalar/breakthrough.pl
@@ -1,3 +1,8 @@
+
+% **********************************************************************
+% **********************************************************************
+
+% Inicializa o tabuleiro.
 initBoard([
 	   [ 1, 1, 1, 1, 1, 1, 1, 1 ],
 	   [ 1, 1, 1, 1, 1, 1, 1, 1 ],
@@ -10,7 +15,10 @@ initBoard([
 	  ]).
 
 
-% Imprime a peça do jogador correspondente
+% **********************************************************************
+% **********************************************************************
+
+% Imprime a peça do jogador correspondente.
 writePlayer(1) :-
 	write('1').
 writePlayer(2) :-
@@ -18,24 +26,50 @@ writePlayer(2) :-
 writePlayer(0) :-
 	write(' ').
 
-% Imprime elementos da sublista
-printRow([])    :-
+	
+% **********************************************************************
+% **********************************************************************
+
+% Critério de paragem.
+% Imprime também a borda direita da última célula de cada linha.
+printRow([])    :- 
 	write('| ').
+
+% Imprime a borda esquerda da célula, e a respectivo
+% peça do jogador (cabeça da lista).
+% Por fim chama recursivamente a mesma função com
+% a cauda da lista, até encontrar o critério de paragem.
 printRow([H|T]) :-
 	write('| '),
 	writePlayer(H),
 	write(' '),
 	printRow(T).
+	
 
-% Imprimie a linha de separação horizontal
+% **********************************************************************
+% **********************************************************************
+
+% Imprimie a linha de separação horizontal.
 printHorizSep   :-
 	write('   --- --- --- --- --- --- --- ---').
-% Imprimie os números das colunas
+
+% Imprimie os números das colunas.
 printColNumbers :-
 	write('    1   2   3   4   5   6   7   8').
 
-%Acrescenta número da linha no início e no fim da sublista
+
+% **********************************************************************
+% **********************************************************************
+	
+% Critério de paragem.
 printFullRow([], _).
+
+% Imprime o número da linha no ínicio e no fim de cada iteração,
+% bem como o separador horizontal. Chama também o predicado que irá
+% imprimir todas as celulas de cada linha, com a respectiva peça.
+% Por fim chama recursivamente a mesma função com a cauda da
+% lista, e o número actual da linha, até atingir o critério de paragem,
+% ou seja, quando a a cauda for uma lista vazia.
 printFullRow([H|T], N) :-
 	N1 is N+1,
 	printHorizSep,
@@ -46,9 +80,18 @@ printFullRow([H|T], N) :-
 	write(N),
 	nl,
 	printFullRow(T, N1).
+	
+		
+% **********************************************************************
+% **********************************************************************
 
-%Imprime tabuleiro,  com as linhas e colunas numeradas
+% Regra a ser usada quando é passado um tabuleiro vazio.
 printBoard([]).
+
+% Imprime o tabuleiro, chamando o predicado que imprime o número
+% das colunas (no início e no fim do tabuleiro),
+% e de seguida o predicado que imprime cada linha individual.
+% É imprimido também o separador horizontal do tabuleiro.
 printBoard([H|T]) :-
 	printColNumbers,
 	nl,
@@ -57,12 +100,18 @@ printBoard([H|T]) :-
 	nl,
 	printColNumbers.
 
-%Imprime tabuleiro inicial
+	
+% **********************************************************************
+% **********************************************************************
+
+% Inicializa e imprime tabuleiro no estado inicial.
 init :-
 	initBoard(A),
 	printBoard(A).
 	
 	
+% **********************************************************************
+% **********************************************************************
 	
 % Move C1R1 para C2R2
 %movePawn([Ox,Oy], [Dx,D2]).
